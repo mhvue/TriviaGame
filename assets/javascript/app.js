388334d0592ac3d4d0884932845bbbaa7c723b02
Mai Houa Vue <mhvue4@gmail.com>
2020-01-11 18:04:43 -0600

    changed picture posiiton for when time up

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,7 +59,7 @@ var questions= [
 //console.log(questions[0].question);
 //console.log(questions[0].answer);
 //console.log(questions[0].choices)
-console.log(questions.length);
+//console.log(questions.length);
 
 
 
@@ -77,60 +77,51 @@ var timerOn=false;
 var count=0;
 
 
+//list of Functions here: 
+
 //function to start game
 function startGame() {
-   
-    $("#question").html("<h2>" + questions[count].question + " </h2>").show();
-    for (var i = 0; i < questions[count].choices.length; i++) {
-        $("#choice" + i).html("<input type='submit' value='" + questions[count].choices[i] + "'> ");
-     };
-  $("#choices-container").show();
-   userAnswers();
+  timerOn=true;
+  
+  $("#question").html("<h1>" + questions[count].question + " </h1>").show();
 
+  for (var i = 0; i < questions[count].choices.length; i++) {
+    $("#choice" + i).html("<input type='submit' value='" + questions[count].choices[i] + "'> ");
+  };
 
-
+  userAnswers();
+  $("#choices-container, #timer").show();
 };
-//startGame(); testing out this function by calling it
 
-
-//this function lets the time goes down
+//this function lets the time actually go down
 function setTime () { 
     intervalId= setInterval(runTimer, 1000);
 };
 
-
-    
-//function to run the timer so it goes down (hence --)
+//function to run the timer so it goes down 
 function runTimer () {
     timer--;
-    $("#timer").html("<h2> Time: " + timer + " seconds. </h2>");
+    $("#timer").html("<h1> Time: " + timer + " seconds. </h1>");
     
-//need this IF here b/c need to know when timer = 0 on what to do..
-//if player taking too long to answer, show msg of Times up, 
-//display correct answer
-//go on the next page automatically
+//need this IF here b/c need to know when timer = 0
     if(timer === -1) { 
       unAnswered++;
-      $("#question").hide();
+      $("#question, #choices-container").hide();
+     // $("#question").hide();
       $("span").children().hide();
-  
-      var timeDiv = $("<div>"); // create a new div to hold msg for times up
+      
+      var timeDiv = $("<div>"); // create a new div to hold msg for when times =0
       timeDiv.addClass("time-container");
-      $("#timer").html(timeDiv); //def need this!
-      $(".time-container").html("<h1 class='textUpdates'>  Uh Oh..Time ran out! <br>" + "Answer is: " + questions[count].answer + "</h1>"+ "<img src=' " + questions[count].picture + "' width='300px' > ");
+      $("#timer").html(timeDiv); 
+      $(".time-container").html("<h1 id='timeOutText'>  Uh Oh..Time ran out! <br></h1>" + "<h1 id= 'timeOutAnswerText'>Answer is: " + questions[count].answer + "</h1>"+ "<img id='timeUpImg' src=' " + questions[count].picture + "' width='300px' > ");
       clearTimer();
       setTimeout(nextQuestion,3000);
-      
-     
     }
-
-    //console.log(unAnswered);
 };
 
-
-//collect user's guess 
+//collect user's guess and compared to answer 
 function userAnswers() {
-  $("span").unbind("click").on("click", function (){ //had to unbind b/c another click event was making answers increment per question
+  $("span").unbind("click").on("click", function (){ //had to unbind click event was making answers increment per question
     userGuess=$(this).children().val();
     //console.log(userGuess);
     //console.log("clicked!");
@@ -142,51 +133,49 @@ function userAnswers() {
   if (userGuess === triviaAnswer) {
     userCorrect++;
     $("span").children().hide();
+    $("#question, #choices-container, #timer").hide();
     var correctDiv = $("<div>"); // create a new div to show correct answer to user
     correctDiv.addClass("msg-container");
-    $("#messages").html(correctDiv); //def need this!
-    $(".msg-container").html("<h1 class ='answerHeader'> " + userGuess + " is correct!</h1>" + "<img src=' " + questions[count].picture + "' width='300px'>");
+    $("#messages").html(correctDiv);
+    $(".msg-container").html("<h1 class ='answerHeader'> " + userGuess + " is correct!</h1>" + "<img class='answerImg' src=' " + questions[count].picture + "' width='300px'>");
     clearTimer();
     setTimeout(nextQuestion,3000);
     }
   
 //if answered incorrectly, showed msg of incorrect and include answer the go on next question
   else{
-      //console.log("wrong");
     userIncorrect++;
     $("span").children().hide();
+    $("#question, #choices-container, #timer").hide();
     var wrongDiv = $("<div>"); //create new div to hold msg of incorrect to user
     wrongDiv.addClass("wrongMsg-container");
     $("#messages").append(wrongDiv); 
-    $(".wrongMsg-container").html("<h1 class='answerHeader'><i> " + userGuess + " is incorrect!</i> Correct answer is: " + triviaAnswer + " </h1> <img src=' " + questions[count].picture + "' width='300px'>");
-    clearTimer();
+    $(".wrongMsg-container").html("<h1 class='answerHeader'><i> " + userGuess + " is incorrect!</i> Correct answer is: " + triviaAnswer + " </h1> <img class='answerImg' src=' " + questions[count].picture + "' width='300px'>");
+    clearTimer(); 
     setTimeout(nextQuestion,3000);
   }
-
   //console.log(userCorrect);
   //console.log(userIncorrect);
 });
 }
 
-//userAnswers(); testing out this function by calling it
 
 //function to go through each question 
 function nextQuestion () {
-    $(".msg-container").hide();
-    $(".wrongMsg-container").hide();
+    $(".msg-container, .wrongMsg-container").hide();
     $("#span").children().show();
-    $("#question").show();
+    $("#question, #timer").show();
     count++;
     timer=5;
 
     if (count === questions.length) {
-      console.log("End");
       clearTimer();
       endScreen();
       restart();
-    }else{
+    }
+    else{
       setTime(); //def need this here for questions to continue on to next 
-      $("#timer").html("<h2> Time: " + timer + " seconds. </h2>");
+      $("#timer").html("<h2 class='timeHeader'> Time: " + timer + " seconds. </h2>");
       startGame(); //this needs to stay down (last) for it to work 
     };
 };
@@ -195,62 +184,56 @@ function nextQuestion () {
 //function to clear the timer 
 function clearTimer () {
     clearInterval(intervalId);
-    //once answer is clicked (rather wrong or correct,we need to reset timer)
 };
 
 //function restart game 
 function restart () {
-  $("#choices-container").hide();
- $("#question").hide();
- $("#timer").hide();
-
-  $("#start").show(); //show button again and if click on start game
-  $("#start").on("click", function (){
-    count=0;
-    startGame();
-    //hide start button
-    $("#start").hide();
-  
-    timer=6;
-    $("#timer").html("<h2> Time: " + timer + " seconds. </h2>");
-   
-    
-    
+  timerOn=false;
+  $("#startButton").show();
+  $("#startButton").on("click", function () {
+    $("#scoreboard").hide();
+    $("#startButton").hide();
+    timer=5;
+    setTime();
+    $("#timer").html("<h2 class='timeHeader'> Time: " + timer + " seconds. </h2>");
+    //$("#friendsThemeSong")[0].pause(); //not working
+   // $("#friendsThemeSong")[0].currentTime =0; //obtained from Medium.com about adding audio
  
-  });
-}
+
+  count=0;
+  startGame(); //try to use setTimeout again
+});
+  userCorrect = 0;
+  userIncorrect = 0;
+  unAnswered = 0;
+};
 
 function endScreen () {
-  timerOn=true;
-  $("#scoreboard").html("<h1> Correct: " + userCorrect + "<br>" + "Incorrect: " + userIncorrect + "<br>"+ "Unanswered: " + unAnswered + "<br> Want to play again? If Yes, press Start! </h1>"); 
-
-}
-//console.log(endScreen());
-//End screen:
-
-//at the final screen, show correct answer and incorrect answers
- //at the final screen: offer to restart with click of button in which game would start automatically again 
-
-
+  timerOn=false;
+  $("#choices-container, #question, #timer").hide();
+  console.log("trying to hide");
+  $("#scoreboard").html("<h1 id='endText'> <u>Results: </u> <br><br> Correct: " + userCorrect + "<br>" + "Incorrect: " + userIncorrect + "<br>"+ "Unanswered: " + unAnswered + "<br> Press Start to play again! </h1>");
+ 
+  //$("#friendsThemeSong")[0].pause();
+};
 
 
 //below here is how the game works or action of the game:
 //welcome page with on click button to start the game 
 
-
 $(document).ready(function () {
   $("#choices-container, #question, #timer").hide();
+  
+    
+$("#startButton").one("click", function () {// .one for so click is not "adding" on to each other (per advice from friend)
+  //console.log("click");
+  //hide start button
+  $("#startButton").hide();
+  //$("#friendsThemeSong")[0].play(); comment out while fixing page
+  $("#yellowFrame").fadeOut("slow");
+  setTimeout(startGame, 1000);
+  setTime();
 
-    
-$("#start").one("click", function () {
-  console.log("click");
-    //hide start button
-    $("#start").hide();
-   $("#yellowFrame").fadeOut("slow");
-   $("#choices-container, #question, #timer").show();
-    setTimeout(startGame,1000);
-    setTime();
-    
 });
 
 
